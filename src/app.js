916bef96e1560a56f5423352c8a7660c0c8b34bd
carmachado <cmachado.dev@gmail.com>
2020-08-17 20:04:33 -0300

    Implemented routes to repository package

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require('uuid');
 
 // const { uuid } = require("uuidv4");
 
@@ -12,22 +13,66 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuidv4(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const id = request.params.id;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+
+  repository = repositories[repositoryIndex];
+
+  repositories[repositoryIndex] = { ...repository, title, url, techs };
+
+  response.sendStatus(204);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const id = request.params.id;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+  
+  repositories.splice(repositoryIndex);
+
+  response.sendStatus(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  // TODO  
+  const id = request.params.id;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repository not found'});
+  }
+  
+  repositories[repositoryIndex].likes++;
+
+  response.sendStatus(204)
 });
 
 module.exports = app;
